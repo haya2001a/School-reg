3828a45ad31431abe421d944df29fcd3d9e41dc1
haya2001a <hayaalami0@gmail.com>
2023-01-08 15:40:51 +0200

    Adding new Task

---
+++ js/java.js
@@ -0,0 +1,123 @@
+var type=document.getElementById('select_type');
+//classes : 
+class person{
+    constructor(Name,dob,gender,birthcity){
+    this.Name=Name;
+    this.dob=dob;
+    this.gender=gender;
+    this.birthcity=birthcity;
+    }
+}
+class student extends person{
+    constructor(Name,dob,gender,birthcity,studentclass){
+        super(Name,dob,gender,birthcity);
+        this.studentclass=studentclass;
+    }
+    
+}
+class empolyee extends person{
+    constructor(Name,dob,gender,birthcity,empsalary){
+        super(Name,dob,gender,birthcity);
+        this.empsalary=empsalary;
+    }
+}
+//To show and hide fields : 
+function showField(){
+    
+    if(type.value=='s'){
+        document.getElementById('e_salary').style.visibility="hidden";
+        document.getElementById('current_class').style.visibility="visible";
+
+    }else{
+        document.getElementById('current_class').style.visibility="hidden";
+        document.getElementById('e_salary').style.visibility="visible";
+
+    }
+}
+// add students to the table 
+function studentAddToList(student){
+   const st_tbl=document.getElementById('student_list');
+   const row=document.createElement('tr');
+   row.innerHTML=`
+   <td>${student.Name}</td>
+   <td>${student.dob}</td>
+   <td>${student.gender}</td>
+   <td>${student.birthcity}</td>
+   <td>${student.studentclass}</td>
+   `;
+   st_tbl.appendChild(row);
+
+}
+
+function clearin(){
+    document.getElementById('name').value='';
+    document.getElementById('bod').value='';
+    document.getElementById('student_class').value='';
+    document.getElementById('emp_sal').value='';
+    document.getElementById('gender_select').value='';
+
+    
+
+}
+
+//add employee tables : 
+function empAddToList(empolyee){
+    const employee_tbl=document.getElementById('emp_tbl');
+    const row=document.createElement('tr');
+    row.innerHTML=`
+    <td>${empolyee.Name}</td>
+    <td>${empolyee.dob}</td>
+    <td>${empolyee.gender}</td>
+    <td>${empolyee.birthcity}</td>
+    <td>${empolyee.empsalary}</td>
+    `;
+    employee_tbl.appendChild(row); 
+ }
+document.getElementById('insert_form').addEventListener('submit',function(e){
+    let students=[];
+    let empoloyees=[];
+    const name=document.getElementById('name').value;
+    const dob=document.getElementById('bod').value;
+    const gender=document.getElementById('gender_select').value;
+    var t = document.getElementById('city_select');
+    const city = t.options[t.selectedIndex].text;
+    var v = document.getElementById('select_type');
+    const type = v.options[v.selectedIndex].text;
+    const student_class=document.getElementById('student_class').value;
+    const empolyee_sal=document.getElementById('emp_sal').value;
+    
+    
+
+if(name==='' || dob==='' ||gender==='Gender' ||city==='Your Birth Cirty' ||type==='Person Type'){
+    
+    swal('oops...','you have to fill all fields !','error');  
+    e.preventDefault(); 
+}
+else if(type==='Student' &&student_class===''){
+    swal('oops...','you have to enter a Class name !','error');  
+    e.preventDefault(); 
+ }else if(type==='Employee' &&empolyee_sal===''){
+    swal('oops...','U have to enter the Salary !','error');  
+    e.preventDefault(); 
+}else{
+    if(type==='Student'){
+        const student1=new student(name,dob,gender,city,student_class);
+        studentAddToList(student1);
+        students.push(student1);
+        console.log(students);
+    }else{
+        const emp=new empolyee(name,dob,gender,city,empolyee_sal);
+        //console.log(student1);
+        //console.log(name,bod,gender,city,type,student_class);
+        empAddToList(emp);
+        empoloyees.push(emp);
+        //console.log('im employeeeeeee');
+    }
+    clearin();
+    e.preventDefault();
+}
+e.preventDefault();
+
+})
+
+
